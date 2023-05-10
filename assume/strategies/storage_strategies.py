c054b30210f8e01b97590e28f4dfc2edc5ad161e
unknown <adamsjo@web.de>
2023-05-10 15:47:14 +0200

    Adjustet storage_strategy to fit flexABLE

--- assume/strategies/storage_strategies.py
+++ assume/strategies/storage_strategies.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 from assume.strategies.base_strategy import BaseStrategy
 from assume.units.storage_unit import StorageUnit
 
@@ -9,16 +12,19 @@ class NaiveStorageStrategy(BaseStrategy):
         self,
         unit: StorageUnit = None,
         operational_window: dict = None,
+        price_forecast: pd.Series = None,
+        current_time: pd.Timestamp = None
     ):
         """
         Takes information from a unit that the unit operator manages and
         defines how it is dispatched to the market
 
         Return: volume, price
+        Strategy analogue to flexABLE
 
         """
-        #bids depent of current SOC 
-        if unit.current_SOC < (unit.max_SOC + unit.min_SOC) / 2:
+        average_price = np.mean(price_forecast)
+        if price_forecast[current_time] > average_price:
             #place bid to discharge
             price = operational_window["max_power_discharge"]["marginal_cost"]
             volume = operational_window["max_power_discharge"]["power_discharge"]
