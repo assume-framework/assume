c5c1d0aa825e44d7aa090878831dcbea80b205be
Florian Maurer <maurer@fh-aachen.de>
2023-08-02 15:00:14 +0200

    add other clearing mechanisms * all_or_nothing * pyomo from ramiz

---
+++ assume/markets/clearing_algorithms/__init__.py
@@ -0,0 +1,13 @@
+from assume.common.market_objects import market_mechanism
+
+from .all_or_nothing import pay_as_bid_aon, pay_as_clear_aon
+from .base import pay_as_bid, pay_as_clear
+from .pyomo_clearing import nodal_pricing_pyomo
+
+clearing_mechanisms: dict[str, market_mechanism] = {
+    "pay_as_clear": pay_as_clear,
+    "pay_as_bid": pay_as_bid,
+    "pay_as_bid_all_or_nothing": pay_as_bid_aon,
+    "pay_as_clear_all_or_nothing": pay_as_clear_aon,
+    "nodal_pricing_pyomo": nodal_pricing_pyomo,
+}
