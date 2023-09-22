a50bf4549bb22ab3a5f7b45b605656745adce9fd
Florian Maurer <maurer@fh-aachen.de>
2023-09-22 11:07:59 +0200

    Add datetime serializer * fix pandas warning in latest version

--- assume/common/mango_serializer.py
+++ assume/common/mango_serializer.py
@@ -1,6 +1,18 @@
+import calendar
 import pickle
+from datetime import datetime
 
 from mango.messages.codecs import JSON, GenericProtoMsg
+
+
+def datetime_json_serializer():
+    def __tostring__(dt: datetime):
+        return calendar.timegm(dt.utctimetuple())
+
+    def __fromstring__(dt: datetime):
+        return datetime.utcfromtimestamp(dt)
+
+    return datetime, __tostring__, __fromstring__
 
 
 def generic_json_serializer():
@@ -30,5 +42,6 @@ def generic_pb_serializer():
 
 def mango_codec_factory():
     codec = JSON()
+    codec.add_serializer(*datetime_json_serializer())
     codec.add_serializer(*generic_json_serializer())
     return codec
