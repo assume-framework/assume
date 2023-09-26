05ff19ec84fb1f4675babf6c1d0316b4f47c0a95
Johanna Adams <johanna.adas@tf.uni-freiburg.de>
2023-09-26 15:38:01 +0200

    remove "price_EOM" from learning_strategies.py

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -45,7 +45,6 @@ def test_init_function(storage_unit):
     assert storage_unit.ramp_up_charge == -60
     assert storage_unit.ramp_up_discharge == 60
     assert storage_unit.initial_soc == 0.5
-    assert storage_unit.outputs["soc"][0] == 0.5
 
 
 def test_reset_function(storage_unit):
