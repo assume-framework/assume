f535b5687646fc9838e7fd82757b92f0c7e5c4f4
Florian Maurer <maurer@fh-aachen.de>
2023-09-29 22:13:44 +0200

    update version to 0.2 - fix dependency

--- assume/__init__.py
+++ assume/__init__.py
@@ -8,5 +8,5 @@ from assume.world import World
 
 __version__ = "0.0.1"
 
-__author__ = "ASSUME Developers: Nick Harder, Kim Miskiw, FLorian Mauer, Manish Khanra"
+__author__ = "ASSUME Developers: Nick Harder, Kim Miskiw, Florian Maurer, Manish Khanra"
 __copyright__ = "AGPL-3.0 License"
