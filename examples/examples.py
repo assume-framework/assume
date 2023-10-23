a825883bf93c6201226e88305a947edb1e9a364d
khm <manishkhanra@gmail.com>
2023-10-23 13:17:54 +0200

    ..

--- examples/examples.py
+++ examples/examples.py
@@ -62,8 +62,8 @@ if __name__ == "__main__":
     - local_db: without database and grafana
     - timescale: with database and grafana (note: you need docker installed)
     """
-    data_format = "timescale"  # "local_db" or "timescale"
-    example = "rl"
+    data_format = "local_db"  # "local_db" or "timescale"
+    example = "small_with_plant"
 
     if data_format == "local_db":
         db_uri = f"sqlite:///./examples/local_db/assume_db_{example}.db"
