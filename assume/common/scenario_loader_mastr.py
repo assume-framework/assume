mastr_scenario: fix unit conversion fixes various bugs caches renewables timeseries once queried

assume/common/scenario_loader_mastr.py
@@ -3,7 +3,9 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 import logging
 import os
+import tempfile
 from datetime import datetime, timedelta
+from pathlib import Path
 
 import pandas as pd
 from dateutil import rrule as rr
@@ -21,18 +23,19 @@ world = World(database_uri=db_uri)
 
 config = ["DEA", "DEB", "DEC", "DED", "DEE", "DEF"]
 config = ["DEA27"]
+config = ["DEA"]
 
 
 async def init():
     year = 2019
     start = datetime(year, 1, 1)
-    end = datetime(year + 1, 1, 1)
+    end = datetime(year + 1, 1, 1) - timedelta(hours=1)
     index = pd.date_range(
         start=start,
-        end=end - timedelta(hours=1),
+        end=end,
         freq="H",
     )
-    sim_id = "world_script_simulation"
+    sim_id = "mastr_scenario"
     print("loading mastr scenario")
 
     database = os.getenv("INFRASTRUCTURE_SOURCE", "timescale.nowum.fh-aachen.de:5432")
@@ -57,6 +60,8 @@ async def init():
             "pay_as_clear",
             [MarketProduct(timedelta(hours=1), 24, timedelta(hours=1))],
             additional_fields=["block_id", "link", "exclusive_id"],
+            maximum_bid_volume=1e9,
+            maximum_bid_price=1e9,
         )
     ]
 
@@ -65,18 +70,59 @@ async def init():
     for market_config in marketdesign:
         world.add_market(mo_id, market_config)
 
+    co2_price = 1
+    fuel_prices = {
+        "hard coal": 8.6,
+        "lignite": 1.8,
+        "oil": 22,
+        "gas": 26,
+        "biomass": 20,
+        "nuclear": 1,
+        "co2": 20,
+    }
+
+    default_strategy = {"energy": "naive"}
+    from assume.strategies.dmas_powerplant import DmasPowerplantStrategy
+
+    world.bidding_strategies["dmas_pwp"] = DmasPowerplantStrategy
+    dmas_strategy = {"energy": "dmas_pwp"}
+    bidding_strategies = {
+        "hard coal": default_strategy,
+        "lignite": default_strategy,
+        "oil": default_strategy,
+        "gas": default_strategy,
+        "biomass": default_strategy,
+        "nuclear": default_strategy,
+        "wind": default_strategy,
+        "solar": default_strategy,
+        "demand": default_strategy,
+    }
+
     # for each area - add demand and generation
     for area in config:
-        print(f"loading config {area}")
-        demand = infra_interface.get_demand_series_in_area(area, year)
-        demand = demand.resample("H").mean()
-        # demand in MW
+        print(f"loading config {area} for {year}")
+        config_path = Path.home() / ".assume" / f"{area}_{year}"
+        if not config_path.is_dir():
+            print(f"query database time series")
+            demand = infra_interface.get_demand_series_in_area(area, year)
+            demand = demand.resample("H").mean()
+            # demand in MW
+            solar, wind = infra_interface.get_renewables_series_in_area(
+                area,
+                start,
+                end,
+            )
+            config_path.mkdir(parents=True, exist_ok=True)
+            demand.to_csv(config_path / "demand.csv")
+            solar.to_csv(config_path / "solar.csv")
+            wind.to_csv(config_path / "wind.csv")
+        else:
+            print(f"use existing local time series")
+            demand = pd.read_csv(config_path / "demand.csv", index_col=0).squeeze()
+            solar = pd.read_csv(config_path / "solar.csv", index_col=0).squeeze()
+            wind = pd.read_csv(config_path / "wind.csv", index_col=0).squeeze()
+
         sum_demand = demand.sum(axis=1)
-        solar, wind = infra_interface.get_renewables_series_in_area(
-            area,
-            start,
-            end,
-        )
 
         world.add_unit_operator(f"demand{area}")
         world.add_unit(
@@ -87,7 +133,7 @@ async def init():
             {
                 "min_power": 0,
                 "max_power": sum_demand.max(),
-                "bidding_strategies": {"energy": "naive"},
+                "bidding_strategies": bidding_strategies["demand"],
                 "technology": "demand",
             },
             NaiveForecast(index, demand=sum_demand),
@@ -102,7 +148,7 @@ async def init():
             {
                 "min_power": 0,
                 "max_power": solar.max(),
-                "bidding_strategies": {"energy": "naive"},
+                "bidding_strategies": bidding_strategies["solar"],
                 "technology": "solar",
             },
             NaiveForecast(
@@ -117,7 +163,7 @@ async def init():
             {
                 "min_power": 0,
                 "max_power": wind.max(),
-                "bidding_strategies": {"energy": "naive"},
+                "bidding_strategies": bidding_strategies["wind"],
                 "technology": "wind",
             },
             NaiveForecast(
@@ -126,16 +172,6 @@ async def init():
         )
 
         world.add_unit_operator(f"conventional{area}")
-        co2_price = 1
-        fuel_prices = {
-            "hard coal": 8.6,
-            "lignite": 1.8,
-            "oil": 22,
-            "natural gas": 26,
-            "biomass": 20,
-            "nuclear": 1,
-            "co2": 20,
-        }
 
         for fuel_type in ["nuclear", "lignite", "hard coal", "oil", "gas"]:
             plants = infra_interface.get_power_plant_in_area(area, fuel_type)
@@ -149,12 +185,12 @@ async def init():
                     f"conventional{area}",
                     # the unit_params have no hints
                     {
-                        "min_power": plant["minPower"],
-                        "max_power": plant["maxPower"],
-                        "bidding_strategies": {"energy": "naive"},
+                        "min_power": plant["minPower"] / 1e3,  # kW -> MW
+                        "max_power": plant["maxPower"] / 1e3,  # kW -> MW
+                        "bidding_strategies": bidding_strategies[fuel_type],
                         "emission_factor": plant["chi"],
                         "efficiency": plant["eta"],
-                        "technology": "wind",
+                        "technology": fuel_type,
                         "start_cost": plant["startCost"],
                     },
                     NaiveForecast(