6b19ae7edf987609613550fd3853fd0f3205db2d
Nick Harder <nick.harder94@gmail.com>
2024-02-22 15:05:43 +0100

    remove pyomo markets -remove dependency on pyomo when also loading markets -maybe we should include pyomo as standard dependency at sme point

--- assume/markets/clearing_algorithms/__init__.py
+++ assume/markets/clearing_algorithms/__init__.py
@@ -6,25 +6,26 @@ from assume.common.market_objects import Orderbook
 from assume.markets.base_market import MarketRole
 
 from .all_or_nothing import PayAsBidAonRole, PayAsClearAonRole
-from .complex_clearing import ComplexClearingRole
-from .complex_clearing_dmas import ComplexDmasClearingRole
-from .nodal_pricing import NodalPyomoMarketRole
 from .simple import PayAsBidRole, PayAsClearRole
 
 clearing_mechanisms: dict[str, MarketRole] = {
     "pay_as_clear": PayAsClearRole,
     "pay_as_bid": PayAsBidRole,
-    "pay_as_clear_complex": ComplexClearingRole,
-    "pay_as_clear_complex_dmas": ComplexDmasClearingRole,
     "pay_as_bid_aon": PayAsBidAonRole,
     "pay_as_clear_aon": PayAsClearAonRole,
-    "nodal_pricing": NodalPyomoMarketRole,
 }
 
 # try importing pypsa if it is installed
 try:
+    from .complex_clearing import ComplexClearingRole
+    from .complex_clearing_dmas import ComplexDmasClearingRole
+    from .nodal_pricing import NodalPyomoMarketRole
     from .redispatch import RedispatchMarketRole
 
+    clearing_mechanisms["pay_as_clear_complex"] = ComplexClearingRole
+    clearing_mechanisms["pay_as_clear_complex_dmas"] = ComplexDmasClearingRole
     clearing_mechanisms["redispatch"] = RedispatchMarketRole
+    clearing_mechanisms["nodal_pricing"] = NodalPyomoMarketRole
+
 except ImportError:
     pass
