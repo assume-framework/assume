12b5cff16dc8507b86e83423b109bc765983082a
Jonathan <sejdija@fh-aachen.de>
2024-04-18 16:52:43 +0200

    reading all data sources

--- assume/scenario/loader_calliope.py
+++ assume/scenario/loader_calliope.py
@@ -22,7 +22,7 @@ import importlib
 # must be installed for import path to work
 import calliope
 
-#model = calliope.Model("path/to/model.yaml", override_dict=override_dict)
+# model = calliope.Model("path/to/model.yaml", override_dict=override_dict)
 model = calliope.examples.urban_scale()
 
 
@@ -68,7 +68,7 @@ async def load_calliope_async(
 
     update_dict_keys(model_data, override_dict)
 
-    if model_data["config"]["mode"] == "plan":
+    if model_data["config"]["build"]["mode"] == "plan":
         raise Exception("Only operate is currently supported in ASSUME")
 
     time_subset = model_data["config"]["init"]["time_subset"]
@@ -77,7 +77,7 @@ async def load_calliope_async(
 
     save_interval = 48
 
-    index = pd.date_range(start=start, end=end, freq="1H")
+    index = pd.date_range(start=start, end=end, freq="1h")
 
     # TODO replace data_sources with actual data
 
@@ -91,35 +91,107 @@ async def load_calliope_async(
 
     # TODO add market operator
 
-    # todo merge techs with data_sources
-    model_data["techs"]
-    for source_name, data_source in model_data["data_sources"].items():
-        # add assume agent
-        await world.add_agent(
-            data_source["agent"],
-            data_source["agent_id"],
-            data_source["agent_name"],
-            data_source["agent_type"],
-            data_source["agent_config"],
-        )
+    agents = []
 
-        world.add_unit_operator(source_name)
-        world.add_unit(
-            f"{source_name}1",
-            "demand",  # TODO - power_plant?
-            source_name,  # units_operator name
-            # the unit_params have no hints
-            {
-                "min_power": 0,
-                "max_power": 1000,
-                "bidding_strategies": {"EOM": "naive_eom"},
-                "technology": "demand",
-                "node": "location1",
-            },
-            NaiveForecast(
-                index, demand=100, availability=1
-            ),  # hier zeitreihen hinterlegen
-        )
+    for source_name, data_source in model_data["data_sources"].items():
+        if data_source["source"]:
+            source_path = data_source["source"]
+            if data_source.get("drop"):
+                skip = 1
+                header = 0
+            else:
+                skip = 0
+                if isinstance(data_source["columns"], list):
+                    header = len(data_source["columns"])
+                else:
+                    header = 0
+            df = pd.read_csv(
+                model_path / source_path,
+                header=header,
+                skiprows=skip,
+                parse_dates=True,
+                index_col=0,
+            )
+        else:
+            df = pd.DataFrame(index=index)
+
+        # TODO don't know what this is for
+        # data_source now has the actual tech
+        tech = data_source["add_dimensions"]["techs"]
+
+        translate_dict = {}  # TODO
+
+        # amiris annotation
+        #         {
+        #     "NUCLEAR": "nuclear",
+        #     "LIGNITE": "lignite",
+        #     "HARD_COAL": "hard coal",
+        #     "NATURAL_GAS": "natural gas",
+        #     "OIL": "oil",
+        #     "HYDROGEN": "hydrogen",
+        #     "Biogas": "biomass",
+        #     "PV": "solar",
+        #     "WindOn": "wind_onshore",
+        #     "WindOff": "wind_offshore",
+        #     "RunOfRiver": "hydro",
+        #     "Other": "other",
+        # }
+
+        world.unit_types.keys()
+
+        for column in df.columns:
+            tech, node = column
+
+        tech_dict = model_data["techs"][tech]
+
+        match tech_dict["base_tech"]:
+            case "supply":
+                # here we know exactly which parameters exist
+                pass
+            case "conversion":
+                max_power = tech_dict["flow_cap_max"]["data"]
+                efficiency = tech_dict["flow_out_eff"]["data"]
+                # TODO durch mögliche colums iterieren
+                agents.append(
+                    {
+                        "agent": df.columns[0] + "random",
+                        "node": node,
+                        "technology": actual_tech,
+                    }
+                )
+            case "demand":
+                max_power = df["demand_heat", "X1"].max()
+                availability = df["demand_heat", "X1"] / max_power
+                min_power = 0
+                for column in df.columns:
+                    tech, node = column
+                actual_tech, product_type = translate_dict.get(tech)
+                # TODO check if product type is demand or actual tech
+                agents.append(
+                    {
+                        "agent": df.columns,
+                        "node": node,
+                        "technology": actual_tech,
+                    }
+                )
+
+                world.add_unit_operator(source_name)
+                world.add_unit(
+                    f"{source_name}1",
+                    "demand",  # TODO - power_plant?
+                    source_name,  # units_operator name
+                    # the unit_params have no hints
+                    {
+                        "min_power": min_power,
+                        "max_power": max_power,
+                        "bidding_strategies": {"EOM": "naive_eom"},
+                        "technology": "demand",
+                        "node": "location1",
+                    },
+                    NaiveForecast(
+                        index, demand=df["demand_heat", "X1"], availability=1
+                    ),  # hier zeitreihen hinterlegen
+                )
 
 
 if __name__ == "__main__":
