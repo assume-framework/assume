b35e46c0b8200f5c9a2d6e184a835d6dd3c58eeb
Nick Harder <nick.harder94@gmail.com>
2024-06-25 11:18:40 +0200

    fix tests

--- tests/test_learning_role.py
+++ tests/test_learning_role.py
@@ -36,7 +36,7 @@ def test_learning_init():
         "early_stopping_threshold": 0.05,
     }
     # test init
-    learn = Learning(learning_config, start, end)
+    learn = Learning(learning_config)
     assert len(learn.rl_strats) == 0
 
     # we need to add learning strategies first
