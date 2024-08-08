efea877d26ae198c9ee3a5f4e75df97334acac4a
Nick Harder <56074305+nick-harder@users.noreply.github.com>
2024-08-08 12:56:09 +0200

    Fix loader and examples (#399)
    ## Description
    - check if availability_df containt values >1 and warn user if so
    - fix these values automatically by normalizing them
    - also check in the power plant itself when calculating max_power if
    availability is larger than max_power
    - restructure examples for easier understanding
    - pack all feature example to example_01 and all learning to example_02
    - add a full year DRL scenario from the fit paper
    - all year examples are now as example_03
    
    ## Checklist
    Please check all applicable items:
    
    - [x] Code changes are sufficiently documented (docstrings, inline
    comments, `doc` folder updates)
    - [ ] New unit tests added for new features or bug fixes
    - [x] Existing tests pass with the changes
    - [x] Reinforcement learning examples are operational (for DRL-related
    changes)
    - [x] Code tested with both local and Docker databases
    - [x] Code follows project style guidelines and best practices
    - [x] Changes are backwards compatible, or deprecation notices added
    - [ ] New dependencies added to `pyproject.toml`
    - [x] A note for the release notes `doc/release_notes.rst` of the
    upcoming release is included
    - [x] Consent to release this PR's code under the GNU Affero General
    Public License v3.0
    
    ---------
    
    Co-authored-by: kim-mskw <kim.miskiw@kit.edu>
    Co-authored-by: Florian Maurer <maurer@fh-aachen.de>

--- examples/examples.py
+++ examples/examples.py
@@ -16,20 +16,21 @@ csv_path = ""
 os.makedirs("./examples/local_db", exist_ok=True)
 
 availabe_examples = {
+    # small examples for easier understanding of different features and configurations
     "small": {"scenario": "example_01a", "study_case": "base"},
     "small_dam": {"scenario": "example_01a", "study_case": "dam"},
     "small_with_opt_clearing": {
         "scenario": "example_01a",
         "study_case": "dam_with_complex_clearing",
     },
-    "small_with_BB_and_LB": {
-        "scenario": "example_01c",
-        "study_case": "dam_with_complex_opt_clearing",
-    },
     "small_with_vre": {"scenario": "example_01b", "study_case": "base"},
     "small_with_vre_and_storage": {
         "scenario": "example_01c",
         "study_case": "eom_only",
+    },
+    "small_with_BB_and_LB": {
+        "scenario": "example_01c",
+        "study_case": "dam_with_complex_opt_clearing",
     },
     "small_with_vre_and_storage_and_complex_clearing": {
         "scenario": "example_01c",
@@ -51,26 +52,41 @@ availabe_examples = {
         "scenario": "example_01d",
         "study_case": "zonal_case",
     },
-    "small_with_steel_plant": {
-        "scenario": "example_04",
-        "study_case": "base_case_steel_plant_2019",
+    # example_01e is used in the tutorial notebook #3: Custom unit and bidding strategy example
+    "market_study_eom": {
+        "scenario": "example_01f",
+        "study_case": "eom_case",
     },
-    "large_2019_eom": {"scenario": "example_02", "study_case": "base_case_2019"},
-    "large_2019_eom_crm": {
-        "scenario": "example_02",
-        "study_case": "eom_crm_case_2019",
+    "market_study_eom_and_ltm": {
+        "scenario": "example_01f",
+        "study_case": "ltm_case",
     },
-    "large_2019_day_ahead": {
-        "scenario": "example_02",
-        "study_case": "dam_case_2019",
-    },
+    # example_01f is used in the tutorial notebook #5: Market configuration comparison example
+    # example_01g is used in the tutorial notebook #6: Advanced order types example
+    #
+    # DRL references case for learning advancement testing
     "small_learning_1": {"scenario": "example_02a", "study_case": "base"},
     "small_learning_2": {"scenario": "example_02b", "study_case": "base"},
     "small_learning_3": {"scenario": "example_02c", "study_case": "base"},
-    "learning_with_complex_bids": {
-        "scenario": "example_02e",
-        "study_case": "base",
+    # Further DRL example simulation showcasing learning features
+    "learning_with_complex_bids": {"scenario": "example_02d", "study_case": "base"},
+    #
+    # full year examples to show real-world scenarios
+    "large_2019_eom": {"scenario": "example_03", "study_case": "base_case_2019"},
+    "large_2019_eom_crm": {
+        "scenario": "example_03",
+        "study_case": "eom_crm_case_2019",
     },
+    "large_2019_day_ahead": {
+        "scenario": "example_03",
+        "study_case": "dam_case_2019",
+    },
+    "large_2019_with_DSM": {
+        "scenario": "example_03",
+        "study_case": "base_case_2019_with_DSM",
+    },
+    "large_2019_rl": {"scenario": "example_03a", "study_case": "base_case_2019"},
+    "large_2021_rl": {"scenario": "example_03b", "study_case": "base_case_2021"},
 }
 
 # %%
@@ -81,7 +97,7 @@ if __name__ == "__main__":
     - timescale: with database and grafana (note: you need docker installed)
     """
     data_format = "local_db"  # "local_db" or "timescale"
-    example = "small_with_steel_plant"
+    example = "small"
 
     if data_format == "local_db":
         db_uri = f"sqlite:///./examples/local_db/assume_db_{example}.db"
