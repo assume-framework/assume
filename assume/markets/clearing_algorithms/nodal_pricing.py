assume-framework/assume

commit 1e8d9ed50f86a028518957cf03c691d5f82a94b0

-add HIGHS as requirement -switch to HIGHS by default instead of GLPK -remove glpk requirements from the git workflows -fix market clearing optimization problem for compatibility -remove pyomo and glpk installations from the notebooks as this is not installed by default

assume/markets/clearing_algorithms/nodal_pricing.py
@@ -71,18 +71,22 @@ class NodalMarketRole(MarketRole):
             loads=self.grid_data["loads"],
         )
 
-        self.solver = marketconfig.param_dict.get("solver", "glpk")
-        self.env = None
+        self.solver = marketconfig.param_dict.get("solver", "highs")
 
+        self.env = None
         if self.solver == "gurobi":
             try:
                 from gurobipy import Env
 
+                self.solver_options = {"LogToConsole": 0}
                 self.env = Env()
                 self.env.setParam("LogToConsole", 0)
             except ImportError:
-                logger.error("gurobi not installed - using GLPK")
-                self.solver = "glpk"
+                logger.error("gurobi not installed - using highs")
+                self.solver = "highs"
+
+        if self.solver == "highs":
+            self.solver_options = {"output_flag": False, "log_to_console": False}
 
         # set the market clearing principle
         # as pay as bid or pay as clear
@@ -160,6 +164,7 @@ class NodalMarketRole(MarketRole):
         status, termination_condition = nodal_network.optimize(
             solver_name=self.solver,
             env=self.env,
+            solver_options={"output_flag": False, "log_to_console": False},
         )
 
         if status != "ok":

assume/markets/clearing_algorithms/redispatch.py
@@ -69,18 +69,22 @@ class RedispatchMarketRole(MarketRole):
             loads=self.grid_data["loads"],
         )
 
-        self.solver = marketconfig.param_dict.get("solver", "glpk")
-        self.env = None
+        self.solver = marketconfig.param_dict.get("solver", "highs")
 
+        self.env = None
         if self.solver == "gurobi":
             try:
                 from gurobipy import Env
 
+                self.solver_options = {"LogToConsole": 0}
                 self.env = Env()
                 self.env.setParam("LogToConsole", 0)
             except ImportError:
-                logger.error("gurobi not installed - using GLPK")
-                self.solver = "glpk"
+                logger.error("gurobi not installed - using highs")
+                self.solver = "highs"
+
+        if self.solver == "highs":
+            self.solver_options = {"output_flag": False, "log_to_console": False}
 
         # set the market clearing principle
         # as pay as bid or pay as clear
@@ -196,6 +200,7 @@ class RedispatchMarketRole(MarketRole):
             status, termination_condition = redispatch_network.optimize(
                 solver_name=self.solver,
                 env=self.env,
+                solver_options=self.solver_options,
             )
 
             if status != "ok":

.github/workflows/lint-pytest.yaml
@@ -59,11 +59,9 @@ jobs:
         reuse lint
     - name: Install dependencies
       run: |
-        sudo apt-get update && sudo apt-get install --no-install-recommends -y coinor-cbc gcc g++ libglpk-dev glpk-utils && sudo rm -rf /var/lib/apt/lists/*
         python -m pip install --upgrade pip
         if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
         python -m pip install -e .[test]
-        python -m pip install pyomo
     - name: Lint with ruff
       run: |
         # stop the build if there are Python syntax errors or undefined names

.github/workflows/publish.yaml
@@ -28,9 +28,8 @@ jobs:
 
     - name: Install dependencies
       run: |
-        sudo apt-get update && sudo apt-get install --no-install-recommends -y coinor-cbc gcc g++ libglpk-dev glpk-utils && sudo rm -rf /var/lib/apt/lists/*
         python -m pip install --upgrade pip
-        python -m pip install .[test,learning,optimization,oeds]
+        python -m pip install .[test,learning,network,oeds]
 
     - name: Lint with ruff
       run: |

assume/markets/clearing_algorithms/complex_clearing.py
@@ -14,6 +14,9 @@ from assume.common.market_objects import MarketConfig, MarketProduct, Orderbook
 from assume.common.utils import check_for_tensors, create_incidence_matrix
 from assume.markets.base_market import MarketRole
 
+# Set the log level to ERROR
+logging.getLogger("pyomo").setLevel(logging.WARNING)
+
 logger = logging.getLogger(__name__)
 
 SOLVERS = ["appsi_highs", "gurobi", "glpk", "cbc", "cplex"]
@@ -27,7 +30,8 @@ def market_clearing_opt(
     with_linked_bids: bool,
     incidence_matrix: pd.DataFrame = None,
     lines: pd.DataFrame = None,
-    solver: str = "glpk",
+    solver: str = "appsi_highs",
+    solver_options: dict = {},
 ):
     """
     Sets up and solves the market clearing optimization problem.
@@ -56,7 +60,7 @@ def market_clearing_opt(
 
         If linked bids are considered, the acceptance of a child bid is bounded by the acceptance of its parent bid.
 
-        The market clearing is solved using pyomo with the specified solver (glpk is used by default).
+        The market clearing is solved using pyomo with the specified solver (HIGHS is used by default).
         If the specified solver is not available, the model is solved using available solver.
         If none of the solvers are available, an exception is raised.
 
@@ -226,45 +230,24 @@ def market_clearing_opt(
 
     model.objective = pyo.Objective(expr=obj_expr, sense=pyo.minimize)
 
-    # check available solvers, gurobi is preferred
-    solvers = check_available_solvers(*SOLVERS)
-    if len(solvers) < 1:
-        raise Exception(f"None of {SOLVERS} are available")
-
-    if solver not in solvers:
-        logger.warning(f"Solver {solver} not available, using {solvers[0]}")
-        solver = SolverFactory(solvers[0])
-    else:
-        solver = SolverFactory(solver)
-
-    if solver.name == "gurobi":
-        options = {"cutoff": -1.0, "MIPGap": EPS}
-    elif solver.name == "cplex":
-        options = {
-            "mip.tolerances.lowercutoff": -1.0,
-            "mip.tolerances.absmipgap": EPS,
-        }
-    elif solver.name == "cbc":
-        options = {"sec": 60, "ratio": 0.1}
-    # elif solver.name == "glpk":
-    #     options = {"tmlim": 60, "mipgap": 0.1}
-    else:
-        options = {}
-
+    solver = SolverFactory(solver)
     # Solve the model
     instance = model.create_instance()
-    results = solver.solve(instance, options=options)
+    results = solver.solve(instance, options=solver_options)
 
-    # fix all model.x to the values in the solution
+    # Fix all model.x to the values in the solution
     if mode == "with_min_acceptance_ratio":
-        # add dual suffix to the model (we need this to extract the market clearing prices later)
+        # Add dual suffix to the model (needed to extract duals later)
         instance.dual = pyo.Suffix(direction=pyo.Suffix.IMPORT_EXPORT)
 
         for bid_id in instance.Bids:
+            # Fix the binary variable to its value
             instance.x[bid_id].fix(instance.x[bid_id].value)
+            # Change the domain to Reals (or appropriate continuous domain)
+            instance.x[bid_id].domain = pyo.Reals
 
-        # resolve the model
-        results = solver.solve(instance, options=options)
+        # Resolve the model
+        results = solver.solve(instance, options=solver_options)
 
     return instance, results
 
@@ -305,8 +288,9 @@ class ComplexClearingRole(MarketRole):
     def __init__(self, marketconfig: MarketConfig):
         super().__init__(marketconfig)
 
-        self.solver = marketconfig.param_dict.get("solver", "glpk")
+        self.define_solver(solver=marketconfig.param_dict.get("solver", "appsi_highs"))
 
+        # Define grid data
         self.nodes = ["node0"]
         self.zones_id = None
         self.incidence_matrix = None
@@ -331,6 +315,30 @@ class ComplexClearingRole(MarketRole):
                 # Nodal Case
                 self.incidence_matrix = create_incidence_matrix(self.lines, buses)
                 self.nodes = buses.index.values
+
+    def define_solver(self, solver: str):
+        # Get the solver from the market configuration
+        if solver == "highs":
+            solver = "appsi_highs"
+
+        # Check if the solver is available and define solver options
+        solvers = check_available_solvers(*SOLVERS)
+        if len(solvers) < 1:
+            raise Exception(f"None of {SOLVERS} are available")
+
+        if solver == "gurobi":
+            solver_options = {"cutoff": -1.0, "MIPGap": EPS, "LogToConsole": 0}
+        elif solver == "appsi_highs":
+            solver_options = {"output_flag": False, "log_to_console": False}
+        else:
+            solver_options = {}
+
+        if solver not in solvers:
+            logger.warning(f"Solver {solver} not available, using {solvers[0]}")
+            solver = solvers[0]
+
+        self.solver = solver
+        self.solver_options = solver_options
 
     def validate_orderbook(self, orderbook: Orderbook, agent_tuple) -> None:
         """
@@ -462,6 +470,7 @@ class ComplexClearingRole(MarketRole):
                 incidence_matrix=self.incidence_matrix,
                 lines=self.lines,
                 solver=self.solver,
+                solver_options=self.solver_options,
             )
 
             if results.solver.termination_condition == TerminationCondition.infeasible:

assume/strategies/dmas_storage.py
@@ -89,7 +89,9 @@ PRICE_FUNCS = {
 }
 
 
-def get_solver_factory(solvers_str=["appsi_highs", "gurobi", "glpk", "cbc", "cplex"]) -> SolverFactory:
+def get_solver_factory(
+    solvers_str=["appsi_highs", "gurobi", "glpk", "cbc", "cplex"],
+) -> SolverFactory:
     """
     Returns the first available solver from the list of solvers
 

assume/units/steel_plant.py
@@ -29,6 +29,9 @@ SOLVERS = ["appsi_highs", "gurobi", "glpk", "cbc", "cplex"]
 
 logger = logging.getLogger(__name__)
 
+# Set the log level to ERROR
+logging.getLogger("pyomo").setLevel(logging.WARNING)
+
 # Mapping of component type identifiers to their respective classes
 dst_components = {
     "electrolyser": create_electrolyser,
@@ -116,7 +119,13 @@ class SteelPlant(SupportsMinMax, DSMFlex):
         solvers = check_available_solvers(*SOLVERS)
         if len(solvers) < 1:
             raise Exception(f"None of {SOLVERS} are available")
+
         self.solver = SolverFactory(solvers[0])
+        self.solver_options = {
+            "output_flag": False,
+            "log_to_console": False,
+            "LogToConsole": 0,
+        }
 
         self.opt_power_requirement = None
         self.flex_power_requirement = None
@@ -409,7 +418,7 @@ class SteelPlant(SupportsMinMax, DSMFlex):
         # switch the instance to the optimal mode by deactivating the flexibility constraints and objective
         instance = self.switch_to_opt(instance)
         # solve the instance
-        results = self.solver.solve(instance, tee=False)  # , tee=True
+        results = self.solver.solve(instance, options=self.solver_options)
 
         # Check solver status and termination condition
         if (results.solver.status == SolverStatus.ok) and (
@@ -452,7 +461,7 @@ class SteelPlant(SupportsMinMax, DSMFlex):
         # switch the instance to the flexibility mode by deactivating the optimal constraints and objective
         instance = self.switch_to_flex(instance)
         # solve the instance
-        results = self.solver.solve(instance, tee=False)  # , tee=True
+        results = self.solver.solve(instance, options=self.solver_options)
 
         # Check solver status and termination condition
         if (results.solver.status == SolverStatus.ok) and (

examples/inputs/example_01c/config.yaml
@@ -27,7 +27,7 @@ eom_only:
         - bid_type
       market_mechanism: pay_as_clear_complex
       param_dict:
-        solver: glpk
+        solver: highs
 
 eom_and_crm:
   start_date: 2019-01-01 00:00
@@ -57,7 +57,7 @@ eom_and_crm:
         - bid_type
       market_mechanism: pay_as_clear_complex
       param_dict:
-        solver: glpk
+        solver: highs
 
     CRM_pos:
       operator: CRM_operator
@@ -120,4 +120,4 @@ dam_with_complex_opt_clearing:
         - min_acceptance_ratio
         - parent_bid_id
       param_dict:
-        solver: glpk
+        solver: highs

commit 5f7e2210c069d85128807ea66808ebb5b352528f

fix undefined solver_options when using a different solver than gurobi or highs
regression from 1e8d9ed5

assume/markets/clearing_algorithms/nodal_pricing.py
@@ -75,8 +75,10 @@ class NodalMarketRole(MarketRole):
         self.solver = marketconfig.param_dict.get("solver", "highs")
         if self.solver == "gurobi":
             self.solver_options = {"LogToConsole": 0, "OutputFlag": 0}
-        elif self.solver == "highs":
+        elif self.solver == "appsi_highs":
             self.solver_options = {"output_flag": False, "log_to_console": False}
+        else:
+            self.solver_options = {}
 
         # set the market clearing principle
         # as pay as bid or pay as clear

assume/markets/clearing_algorithms/redispatch.py
@@ -73,8 +73,10 @@ class RedispatchMarketRole(MarketRole):
         self.solver = marketconfig.param_dict.get("solver", "highs")
         if self.solver == "gurobi":
             self.solver_options = {"LogToConsole": 0, "OutputFlag": 0}
-        elif self.solver == "highs":
+        elif self.solver == "appsi_highs":
             self.solver_options = {"output_flag": False, "log_to_console": False}
+        else:
+            self.solver_options = {}
 
         # set the market clearing principle
         # as pay as bid or pay as clear