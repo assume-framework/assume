4c2b9cf19bc6896907793bb78e95016bc262990e
Nick Harder <nick.harder94@gmail.com>
2024-12-16 13:32:37 +0100

    -revise advanced learning strategy

--- assume/strategies/learning_advanced_orders.py
+++ assume/strategies/learning_advanced_orders.py
@@ -265,91 +265,59 @@ class RLAdvancedOrderStrategy(RLStrategy):
         # 1.1 Get the Observations, which are the basis of the action decision
         # =============================================================================
 
-        # defines bounds of observation space
-        # stays here as it is unit specific, and different forecasts might apply for different units
-        # different handling would require an extra unit loop at learning role intiliazation and unit specific max/min values
-        # further forecasts might change during the simulation if advanced forecasting is used
-        self.max_market_price = max(unit.forecaster[f"price_{market_id}"])
-        self.min_market_price = min(unit.forecaster[f"price_{market_id}"])
-        self.max_residual = max(unit.forecaster[f"residualy_load_{market_id}"])
-        self.min_residual = min(unit.forecaster[f"residual_load_{market_id}"])
-
-        # residual load forecast
-        upper_scaling_factor_res_load = self.max_residual
-        lower_scaling_factor_res_load = self.min_residual
-
-        # price forecast
-        upper_scaling_factor_price = self.max_market_price
-        lower_scaling_factor_price = self.min_market_price
-
-        # total capacity and marginal cost
-        upper_scaling_factor_total_dispatch = unit.max_power
-        # if unit is not running, total dispatch is 0
-        lower_scaling_factor_total_dispatch = 0
-
-        # marginal cost
-        # Obs[2*foresight+1:2*foresight+2]
-        # Could theoretically be negative (eg. subsidies), but assumed to be always positive in the simulation
-        upper_scaling_factor_marginal_cost = self.max_bid_price
-        lower_scaling_factor_marginal_cost = 0
-
         # checks if we are at end of simulation horizon, since we need to change the forecast then
         # for residual load and price forecast and scale them
-        product_len = (end - start) / unit.index.freq
-        if (
-            end_excl + forecast_len
-            > unit.forecaster[f"residual_load_{market_id}"].index[-1]
-        ):
-            scaled_res_load_forecast = min_max_scale(
-                unit.forecaster[f"residual_load_{market_id}"][
-                    -int(product_len + self.foresight - 1) :
-                ],
-                lower_scaling_factor_res_load,
-                upper_scaling_factor_res_load,
+        if end_excl + forecast_len > self.scaled_res_load_obs.index[-1]:
+            scaled_res_load_forecast = self.scaled_res_load_obs.loc[start:]
+
+            scaled_res_load_forecast = np.concatenate(
+                [
+                    scaled_res_load_forecast,
+                    self.scaled_res_load_obs.iloc[
+                        : self.foresight - len(scaled_res_load_forecast)
+                    ],
+                ]
             )
 
         else:
-            scaled_res_load_forecast = min_max_scale(
-                unit.forecaster[f"residual_load_{market_id}"].loc[
-                    start : end_excl + forecast_len
-                ],
-                lower_scaling_factor_res_load,
-                upper_scaling_factor_res_load,
-            )
+            scaled_res_load_forecast = self.scaled_res_load_obs.loc[
+                start : end_excl + forecast_len
+            ]
 
-        if end_excl + forecast_len > unit.forecaster[f"price_{market_id}"].index[-1]:
-            scaled_price_forecast = min_max_scale(
-                unit.forecaster[f"price_{market_id}"][
-                    -int(product_len + self.foresight - 1) :
-                ],
-                lower_scaling_factor_price,
-                upper_scaling_factor_price,
+        if end_excl + forecast_len > self.scaled_pices_obs.index[-1]:
+            scaled_price_forecast = self.scaled_pices_obs.loc[start:]
+            scaled_price_forecast = np.concatenate(
+                [
+                    scaled_price_forecast,
+                    self.scaled_pices_obs.iloc[
+                        : self.foresight - len(scaled_price_forecast)
+                    ],
+                ]
             )
 
         else:
-            scaled_price_forecast = min_max_scale(
-                unit.forecaster[f"price_{market_id}"].loc[
-                    start : end_excl + forecast_len
-                ],
-                lower_scaling_factor_price,
-                upper_scaling_factor_price,
-            )
+            scaled_price_forecast = self.scaled_pices_obs.loc[
+                start : end_excl + forecast_len
+            ]
 
         # get last accepted bid volume and the current marginal costs of the unit
         current_volume = unit.get_output_before(start)
-
         current_costs = unit.calculate_marginal_cost(start, current_volume)
 
         # scale unit outputs
-        scaled_max_power = min_max_scale(
+        # if unit is not running, total dispatch is 0
+        scaled_total_dispatch = min_max_scale(
             current_volume,
-            lower_scaling_factor_total_dispatch,
-            upper_scaling_factor_total_dispatch,
+            0,
+            unit.max_power,
         )
+
+        # marginal cost
+        # Could theoretically be negative (eg. subsidies), but assumed to be always positive in the simulation
         scaled_marginal_cost = min_max_scale(
             current_costs,
-            lower_scaling_factor_marginal_cost,
-            upper_scaling_factor_marginal_cost,
+            0,
+            self.max_bid_price,
         )
 
         # calculate the time the unit has to continue to run or be down
@@ -358,13 +326,9 @@ class RLAdvancedOrderStrategy(RLStrategy):
             must_run_time = max(op_time - unit.min_operating_time, 0)
         elif op_time < 0:
             must_run_time = min(op_time + unit.min_down_time, 0)
-        upper_scaling_factor_must_run_time = max(
-            unit.min_operating_time, unit.min_down_time
-        )
+        scaling_factor_must_run_time = max(unit.min_operating_time, unit.min_down_time)
 
-        scaled_must_run_time = min_max_scale(
-            must_run_time, 0, upper_scaling_factor_must_run_time
-        )
+        scaled_must_run_time = must_run_time / scaling_factor_must_run_time
 
         # concat all obsverations into one array
         observation = np.concatenate(
@@ -372,7 +336,7 @@ class RLAdvancedOrderStrategy(RLStrategy):
                 scaled_res_load_forecast,
                 scaled_price_forecast,
                 np.array(
-                    [scaled_must_run_time, scaled_max_power, scaled_marginal_cost]
+                    [scaled_must_run_time, scaled_total_dispatch, scaled_marginal_cost]
                 ),
             ]
         )
