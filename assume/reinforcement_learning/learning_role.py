70c28a0b0347a3592a36d8cf1d19aaf6f3be1190
Andreas Eppler <A.Eppler97@web.de>
2025-01-07 14:44:30 +0100

    Fixed a small bug that caused initial_exploration to last one episode longer than expected.

--- assume/reinforcement_learning/learning_role.py
+++ assume/reinforcement_learning/learning_role.py
@@ -147,7 +147,7 @@ class Learning(Role):
 
         # if enough initial experience was collected according to specifications in learning config
         # turn off initial exploration and go into full learning mode
-        if self.episodes_done > self.episodes_collecting_initial_experience:
+        if self.episodes_done >= self.episodes_collecting_initial_experience:
             self.turn_off_initial_exploration()
 
         self.initialize_policy(inter_episodic_data["actors_and_critics"])
@@ -299,7 +299,7 @@ class Learning(Role):
         Notes:
             This method is typically scheduled to run periodically during training to continuously improve the agent's policy.
         """
-        if self.episodes_done > self.episodes_collecting_initial_experience:
+        if self.episodes_done >= self.episodes_collecting_initial_experience:
             learning_rate, critic_losses = self.rl_algorithm.update_policy()
             self.write_learning_params_to_output(learning_rate, critic_losses)
 
