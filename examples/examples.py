b1e2fb9def7cd1dea6922ef250fc3fafb3d1c66a
Nick Harder <nick.harder94@gmail.com>
2025-02-20 09:08:32 +0100

    -small adjustements

--- examples/examples.py
+++ examples/examples.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
 
     # select to store the simulation results in a local database or in timescale
     # when using timescale, you need to have docker installed and can access the grafana dashboard
-    data_format = "timescale"  # "local_db" or "timescale"
+    data_format = "local_db"  # "local_db" or "timescale"
 
     # select the example to run from the available examples above
     example = "small_learning_with_storage"
