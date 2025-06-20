a3974c882dd4112a7ea3dff42c5913eacb9b70fb
Parag Patil <paragpatil39@gmail.com>
2025-06-20 14:50:09 +0200

    modified:   examples/examples.py 	new file:   examples/inputs/example_04/industrial_dsm_units_paper.csv 	new example:   examples/inputs/example_05c for redisp storage 3 nodes 	modified:   examples/inputs/example_05e/demand_df.csv 	modified:   examples/inputs/example_05e/industrial_dsm_units.csv 	new file:   examples/inputs/example_05e/storage_units.csv 	new file:   examples/inputs/example_05e/storage_units.csv.license 	renamed:    examples/inputs/example_05f/availability.csv -> examples/inputs/example_05f/availability_df.csv 	modified:   examples/inputs/example_05f/buses.csv 	modified:   examples/inputs/example_05f/demand_df.csv 	modified:   examples/inputs/example_05f/exchanges_df.csv 	modified:   examples/inputs/example_05f/fuel_prices_df.csv 	modified:   examples/inputs/example_05f/lines.csv

--- examples/examples.py
+++ examples/examples.py
@@ -107,10 +107,17 @@ available_examples = {
     },
     # redispatch for DSM validation example
     "redisp_valid_3_nodes": {"scenario": "example_05b", "study_case": "base"},
+    "redisp_storage_3_nodes": {"scenario": "example_05c", "study_case": "base"},
     "redisp_dsm_3_nodes": {"scenario": "example_05e", "study_case": "base"},
     "redisp_valid_2023": {"scenario": "example_05f","study_case": "base",
     },
 }
+
+
+
+
+
+
 
 
 # %%
@@ -126,7 +133,7 @@ if __name__ == "__main__":
     data_format = "local_db"  # "local_db" or "timescale"
 
     # select the example to run from the available examples above
-    example = "redisp_dsm_3_nodes"
+    example = "redisp_storage_3_nodes"
 
     if data_format == "local_db":
         db_uri = "sqlite:///./examples/local_db/assume_db.db"
@@ -165,6 +172,7 @@ if __name__ == "__main__":
     # )
 
     if world.learning_config.get("learning_mode", False):
+
         # run learning if learning mode is enabled
         run_learning(world)
 
