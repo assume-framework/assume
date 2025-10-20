2a1bab6a9290987ae2122a2d5a010b966984778a
Jannik <raskob@fh-aachen.de>
2025-10-20 09:57:03 +0200

    force negative min & max power for demand units
    Signed-off-by: Jannik <raskob@fh-aachen.de>

--- tests/test_units_operator.py
+++ tests/test_units_operator.py
@@ -56,7 +56,7 @@ async def units_operator() -> UnitsOperator:
         "bidding_strategies": {"EOM": NaiveSingleBidStrategy()},
         "technology": "energy",
         "unit_operator": agent_id,
-        "max_power": 1000,
+        "max_power": -1000,
         "min_power": 0,
         "forecaster": NaiveForecast(index, demand=1000),
     }
@@ -96,7 +96,7 @@ async def rl_units_operator() -> RLUnitsOperator:
         "bidding_strategies": {"EOM": NaiveSingleBidStrategy()},
         "technology": "energy",
         "unit_operator": agent_id,
-        "max_power": 1000,
+        "max_power": -1000,
         "min_power": 0,
         "forecaster": NaiveForecast(index, demand=1000),
     }
@@ -182,7 +182,7 @@ async def test_write_learning_params(rl_units_operator: RLUnitsOperator):
         },
         "technology": "energy",
         "unit_operator": "test_operator",
-        "max_power": 1000,
+        "max_power": -1000,
         "min_power": 0,
         "forecaster": NaiveForecast(index, powerplant=1000),
     }
@@ -294,7 +294,7 @@ def test_participate():
         "bidding_strategies": {"wrong_market": NaiveSingleBidStrategy()},
         "technology": "energy",
         "unit_operator": "x",
-        "max_power": 1000,
+        "max_power": -1000,
         "min_power": 0,
         "forecaster": NaiveForecast(index, demand=1000),
     }
@@ -307,7 +307,7 @@ def test_participate():
         "bidding_strategies": {"EOM": NaiveSingleBidStrategy()},
         "technology": "energy",
         "unit_operator": "x",
-        "max_power": 1000,
+        "max_power": -1000,
         "min_power": 0,
         "forecaster": NaiveForecast(index, demand=1000),
     }
@@ -351,7 +351,7 @@ def test_participate_lambda():
         "bidding_strategies": {"EOM": NaiveSingleBidStrategy()},
         "technology": "wind offshore",
         "unit_operator": "x",
-        "max_power": 1000,
+        "max_power": -1000,
         "min_power": 0,
         "forecaster": NaiveForecast(index, demand=1000),
     }
@@ -395,7 +395,7 @@ def test_participate_custom_lambda():
         "bidding_strategies": {"EOM": NaiveSingleBidStrategy()},
         "technology": "energy",
         "unit_operator": "x",
-        "max_power": 1000,
+        "max_power": -1000,
         "min_power": 0,
         "forecaster": NaiveForecast(index, demand=1000),
     }
@@ -445,7 +445,7 @@ async def test_collecting_rl_values(rl_units_operator: RLUnitsOperator):
         },
         "technology": "energy",
         "unit_operator": "test_operator",
-        "max_power": 1000,
+        "max_power": -1000,
         "min_power": 0,
         "forecaster": NaiveForecast(index, powerplant=1000),
     }
