0db0f6e33b694f8a6d2515a350b8f6698a4864d2
Jannik <raskob@fh-aachen.de>
2025-10-30 09:50:14 +0100

    remove index from world.py
    Signed-off-by: Jannik <raskob@fh-aachen.de>

--- assume/world.py
+++ assume/world.py
@@ -9,7 +9,6 @@ import time
 from datetime import datetime
 from pathlib import Path
 
-import pandas as pd
 from mango import (
     RoleAgent,
     activate,
@@ -35,7 +34,6 @@ from assume.common import (
     mango_codec_factory,
 )
 from assume.common.base import LearningConfig
-from assume.common.fast_pandas import FastIndex_from_pd
 from assume.common.forecaster import UnitForecaster
 from assume.common.utils import datetime2timestamp, timestamp2datetime
 from assume.markets import MarketRole, clearing_mechanisms
@@ -184,8 +182,8 @@ class World:
         start: datetime,
         end: datetime,
         simulation_id: str,
-        index: pd.DatetimeIndex,
         save_frequency_hours,
+        lr_logfreq: str = "h",
         bidding_params: dict = {},
         learning_config: LearningConfig = {},
         episode: int = 1,
@@ -201,11 +199,10 @@ class World:
             start (datetime.datetime): The start datetime for the simulation.
             end (datetime.datetime): The end datetime for the simulation.
             simulation_id (str): The unique identifier for the simulation.
-            index (pandas.Series): The index for the simulation.
+            lr_logfreq (str): Log frequency of the learning role
             save_frequency_hours (int): The frequency (in hours) at which to save simulation data.
             bidding_params (dict, optional): Parameters for bidding. Defaults to an empty dictionary.
             learning_config (LearningConfig, optional): Configuration for the learning process. Defaults to an empty configuration.
-            forecaster (Forecaster, optional): The forecaster used for custom unit types. Defaults to None.
             manager_address: The address of the manager.
             **kwargs: Additional keyword arguments.
 
@@ -244,7 +241,7 @@ class World:
             else f"Evaluation Episode {eval_episode}"
         )
 
-        self.index = FastIndex_from_pd(index)
+        self.lr_logfreq = lr_logfreq
 
         self.bidding_params = bidding_params
 
@@ -331,7 +328,7 @@ class World:
                 db_uri=self.db_uri,
                 output_agent_addr=self.output_agent_addr,
                 train_start=self.start,
-                freq=self.index.freq,
+                freq=self.lr_logfreq,
             )
 
         else:
