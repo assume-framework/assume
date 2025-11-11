acdc7e5afb91a556035b0b27febfe32463c24e7a
Jannik <raskob@fh-aachen.de>
2025-11-11 14:01:23 +0100

    check simulation results in the integration test
    Signed-off-by: Jannik <raskob@fh-aachen.de>

--- tests/test_integration_cli.py
+++ tests/test_integration_cli.py
@@ -2,15 +2,35 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
+import pandas as pd
 import pytest
+from pandas.testing import assert_frame_equal
+from sqlalchemy import create_engine
 
 from assume_cli.cli import cli
 
 
 @pytest.mark.slow
 def test_cli():
-    args = "-s example_01a -c tiny -db sqlite:///./examples/local_db/test_mini.db"
+    dburi = "sqlite:///./examples/local_db/test_mini.db"
+    args = f"-s example_01a -c tiny -db {dburi}"
     cli(args.split(" "))
+
+    db = create_engine(dburi)
+    got = {}
+    with db.begin() as conn:
+        got = {
+            "market_meta": pd.read_sql("SELECT * FROM market_meta", conn),
+            "demand_meta": pd.read_sql("SELECT * FROM demand_meta", conn),
+            "exchange_meta": pd.read_sql("SELECT * FROM exchange_meta", conn),
+        }
+    expected = {
+        "market_meta": pd.read_csv("./tests/fixtures/01a_results/market_meta.csv"),
+        "demand_meta": pd.read_csv("./tests/fixtures/01a_results/demand_meta.csv"),
+        "exchange_meta": pd.read_csv("./tests/fixtures/01a_results/exchange_meta.csv"),
+    }
+    for key in expected.keys():
+        assert_frame_equal(got[key], expected[key], check_dtype=False)
 
 
 @pytest.mark.slow
