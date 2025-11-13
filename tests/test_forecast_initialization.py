51aaea31b18c2e36851a43187378f8228c57b513
Jannik <raskob@fh-aachen.de>
2025-11-13 18:41:52 +0100

    make calculations test more accurate
    Signed-off-by: Jannik <raskob@fh-aachen.de>

--- tests/test_forecast_initialization.py
+++ tests/test_forecast_initialization.py
@@ -2,47 +2,22 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-from unittest.mock import MagicMock
 
 import pandas as pd
 import pytest
 
 from assume.common.forecast_initialisation import ForecastInitialisation
 
+fixture_path = "./tests/fixtures/forecast_init"
+
 
 @pytest.fixture
 def forecast_init():
     index = pd.date_range("2022-01-01", periods=4, freq="h")
-    powerplants_units = pd.DataFrame.from_records(
-        [
-            {
-                "name": "Unit 1",
-                "technology": "nuclear",
-                "bidding_EOM": "naive_eom",
-                "fuel_type": "uranium",
-                "emission_factor": 0.0,
-                "max_power": 1000.0,
-                "min_power": 200.0,
-                "efficiency": 0.3,
-                "additional_cost": 10.3,
-                "unit_operator": "Operator 1",
-            }
-        ],
-        index="name",
+    powerplants_units = pd.read_csv(
+        f"{fixture_path}/powerplants_units.csv", index_col="name"
     )
-    demand_units = pd.DataFrame.from_records(
-        [
-            {
-                "name": "demand_EOM",
-                "technology": "inflex_demand",
-                "bidding_EOM": "naive_eom",
-                "max_power": 1000000,
-                "min_power": 0,
-                "unit_operator": "eom_de",
-            }
-        ],
-        index="name",
-    )
+    demand_units = pd.read_csv(f"{fixture_path}/demand_units.csv", index_col="name")
     market_configs = {
         "EOM": {
             "operator": "EOM_operator",
@@ -58,15 +33,10 @@ def forecast_init():
             "market_mechanism": "pay_as_clear",
         }
     }
-    demand_series = [
-        {"datetime": "2022-01-01 00:00:00", "demand_EOM": 100},
-        {"datetime": "2022-01-01 01:00:00", "demand_EOM": 200},
-        {"datetime": "2022-01-01 02:00:00", "demand_EOM": 300},
-        {"datetime": "2022-01-01 03:00:00", "demand_EOM": 400},
-    ]
-    forecasts = pd.DataFrame.from_records(demand_series)
-    forecasts["datetime"] = pd.to_datetime(forecasts["datetime"])
-    fuel_prices = pd.DataFrame()
+    forecasts = pd.read_csv(
+        f"{fixture_path}/forecasts.csv", index_col="datetime", parse_dates=["datetime"]
+    )
+    fuel_prices = pd.DataFrame(index=index, data={"co2": [10]})
     forecast_init = ForecastInitialisation(
         index,
         powerplants_units,
@@ -76,30 +46,26 @@ def forecast_init():
         availability=pd.DataFrame(),
         demand=pd.DataFrame(),
         exchanges=pd.DataFrame(),
-        fuel_prices=pd.DataFrame(),
+        fuel_prices=fuel_prices,
     )
     return forecast_init
 
 
 def test_forecast_init(forecast_init):
-    forecast_init.calculate_market_price_forecast = MagicMock()
-
     forecast_init.calc_forecast_if_needed()
-    assert forecast_init.calculate_market_price_forecast.called
+
+    market_forecast, load_forecast = forecast_init.calculate_market_forecasts()
+    # assert the passed forecast is generated
+    assert list(market_forecast["EOM"]) == [10.3] * 4
+    assert list(load_forecast["EOM"]) == [100, 200, 300, 400]
 
 
 def test_forecast_init_used_forecast(forecast_init):
-    index = pd.date_range("2022-01-01", periods=4, freq="h")
-    price_series = [
-        {"datetime": "2022-01-01 00:00:00", "price_EOM": 1},
-        {"datetime": "2022-01-01 01:00:00", "price_EOM": 2},
-        {"datetime": "2022-01-01 02:00:00", "price_EOM": 3},
-        {"datetime": "2022-01-01 03:00:00", "price_EOM": 4},
-    ]
-    prices = pd.DataFrame.from_records(price_series)
-    prices["datetime"] = pd.to_datetime(prices["datetime"])
-
+    prices = pd.DataFrame(index=forecast_init.index, data={"price_EOM": [1, 2, 3, 4]})
     forecast_init.set_forecast(prices)
-    forecast_init.calculate_market_price_forecast = MagicMock()
     forecast_init.calc_forecast_if_needed()
-    assert not forecast_init.calculate_market_price_forecast.called
+
+    market_forecast, load_forecast = forecast_init.calculate_market_forecasts()
+    # assert the passed forecast is kept
+    assert market_forecast["EOM"].equals(prices["price_EOM"])
+    assert list(load_forecast["EOM"]) == [100, 200, 300, 400]
