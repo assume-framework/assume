9010591332f11bec90650212c6fafd1bfbdd34e1
Jannik <raskob@fh-aachen.de>
2025-11-19 09:53:52 +0100

    use pathlib
    Signed-off-by: Jannik <raskob@fh-aachen.de>

--- tests/test_forecast_initialization.py
+++ tests/test_forecast_initialization.py
@@ -3,13 +3,17 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 
+from pathlib import Path
+
 import pandas as pd
 import pytest
 from pandas._testing import assert_frame_equal, assert_series_equal
 
 from assume.common.forecast_initialisation import ForecastInitialisation
 
-path = "./tests/fixtures/forecast_init"
+path = Path("./tests/fixtures/forecast_init")
+
+parse_date = {"index_col": "datetime", "parse_dates": ["datetime"]}
 
 
 @pytest.fixture
@@ -32,17 +36,13 @@ def forecast_init():
     forecast_init = ForecastInitialisation(
         market_configs=market_configs,
         index=pd.date_range("2019-01-01", periods=24, freq="h"),
-        powerplants_units=pd.read_csv(f"{path}/powerplant_units.csv", index_col="name"),
-        demand_units=pd.read_csv(f"{path}/demand_units.csv", index_col="name"),
-        availability=pd.read_csv(
-            f"{path}/availability.csv", index_col="datetime", parse_dates=["datetime"]
-        ),
-        demand=pd.read_csv(
-            f"{path}/demand.csv", index_col="datetime", parse_dates=["datetime"]
-        ),
-        fuel_prices=pd.read_csv(f"{path}/fuel_prices.csv", index_col="fuel"),
-        lines=pd.read_csv(f"{path}/lines.csv", index_col="line"),
-        buses=pd.read_csv(f"{path}/buses.csv", index_col="name"),
+        powerplants_units=pd.read_csv(path / "powerplant_units.csv", index_col="name"),
+        demand_units=pd.read_csv(path / "demand_units.csv", index_col="name"),
+        availability=pd.read_csv(path / "availability.csv", **parse_date),
+        demand=pd.read_csv(path / "demand.csv", **parse_date),
+        fuel_prices=pd.read_csv(path / "fuel_prices.csv", index_col="fuel"),
+        lines=pd.read_csv(path / "lines.csv", index_col="line"),
+        buses=pd.read_csv(path / "buses.csv", index_col="name"),
     )
     return forecast_init
 
@@ -50,11 +50,7 @@ def forecast_init():
 def test_forecast_init__calc_market_forecasts(forecast_init):
     market_forecast, load_forecast = forecast_init.calculate_market_forecasts()
     # assert the passed forecast is generated
-    expected = pd.read_csv(
-        f"{path}/results/load_forecast.csv",
-        index_col="datetime",
-        parse_dates=["datetime"],
-    )
+    expected = pd.read_csv(path / "results/load_forecast.csv", **parse_date)
     assert_series_equal(
         expected["load_forecast"], load_forecast["EOM"], check_names=False
     )
@@ -63,16 +59,8 @@ def test_forecast_init__calc_market_forecasts(forecast_init):
 
 def test_forecast_init__calc_node_forecasts(forecast_init):
     congestion_signal, rn_utilization = forecast_init.calc_node_forecasts()
-    expected_cgn = pd.read_csv(
-        f"{path}/results/congestion_signal.csv",
-        index_col="datetime",
-        parse_dates=["datetime"],
-    )
-    expected_uti = pd.read_csv(
-        f"{path}/results/renewable_utilization.csv",
-        index_col="datetime",
-        parse_dates=["datetime"],
-    )
+    expected_cgn = pd.read_csv(path / "results/congestion_signal.csv", **parse_date)
+    expected_uti = pd.read_csv(path / "results/renewable_utilization.csv", **parse_date)
     assert_frame_equal(
         expected_cgn,
         congestion_signal,
@@ -90,9 +78,7 @@ def test_forecast_init__calc_node_forecasts(forecast_init):
 
 
 def test_forecast_init__uses_given_forecast(forecast_init):
-    forecasts = pd.read_csv(
-        f"{path}/forecasts.csv", index_col="datetime", parse_dates=["datetime"]
-    )
+    forecasts = pd.read_csv(path / "forecasts.csv", **parse_date)
     forecast_init._forecasts = forecasts
     price_forecast, load_forecast = forecast_init.calculate_market_forecasts()
     assert_series_equal(
