34ed4a294ff9f1979b4f325d395975b4085450dc
gugrimm <155527339+gugrimm@users.noreply.github.com>
2026-04-20 12:00:29 +0200

    pre-commit

--- tests/test_grid_utils.py
+++ tests/test_grid_utils.py
@@ -6,8 +6,8 @@ import pytest
 
 from assume.common.grid_utils import get_supported_solver_linopy
 
+
 @pytest.mark.require_network
 def test_solver_available():
     assert get_supported_solver_linopy() == "highs"
     assert get_supported_solver_linopy("unknown_solver") == "highs"
-    
