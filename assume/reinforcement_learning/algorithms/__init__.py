90ca11273d4f3cee70dd42f848b9f21310897f2e
Harshul-18 <harshulnanda0@gmail.com>
2026-04-26 09:32:40 +0200

    Fixed assume import logic API

--- assume/reinforcement_learning/algorithms/__init__.py
+++ assume/reinforcement_learning/algorithms/__init__.py
@@ -5,11 +5,33 @@
 from torch import nn
 
 from assume.reinforcement_learning.neural_network_architecture import (
-    MLPActor,
     LSTMActor,
+    MLPActor,
 )
 
 actor_architecture_aliases: dict[str, type[nn.Module]] = {
     "mlp": MLPActor,
     "lstm": LSTMActor,
 }
+
+from assume.reinforcement_learning.algorithms.base_algorithm import ( 
+    A2CAlgorithm,
+    RLAlgorithm,
+)
+from assume.reinforcement_learning.algorithms.maddpg import DDPG 
+from assume.reinforcement_learning.algorithms.mappo import PPO 
+from assume.reinforcement_learning.algorithms.matd3 import TD3 
+
+__all__ = [
+    # Base classes
+    "RLAlgorithm",
+    "A2CAlgorithm",
+    # Concrete algorithms
+    "TD3",
+    "DDPG",
+    "PPO",
+    # Actor architectures
+    "actor_architecture_aliases",
+    "MLPActor",
+    "LSTMActor",
+]
