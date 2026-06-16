2fff8b76f20870a396c6749345191a1104318f77
kim-mskw <kim.miskiw@kit.edu>
2026-06-16 14:29:46 +0200

    - clean runs for haicore with different hp

--- run_job.sh
+++ run_job.sh
@@ -4,16 +4,17 @@
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-#SBATCH --job-name=learn_bat         # Name of your job
+#SBATCH --job-name=pp_46   # Name of your job
 #SBATCH --output=logs/output_%j.txt   # Output log file (%j = job ID)
 #SBATCH --error=logs/error_%j.txt     # Error log file
 #SBATCH --time=48:00:00               # Max runtime (hh:mm:ss)
-#SBATCH --partition=normal
+#SBATCH --partition=normal			  # Partition to submit to (adjust as needed)
+#SBATCH --gres=gpu:1
 #SBATCH --ntasks=1                    # Number of tasks
 #SBATCH --mem=64G                     # Memory
 
 # Load modules or activate environment
-source ../assume-case/bin/activate
+source /assume-case-py311/bin/activate
 
 
 # Run your Python script
