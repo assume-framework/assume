1fa3babd13634e0b0cf595cd6b9152cd6c162af6
graf-wronski <carlwanninger@gmx.de>
2026-09-25 16:35:47 +0200

    Fixing colors and adjusting marginal costs.

--- assume/workshop/plot.py
+++ assume/workshop/plot.py
@@ -3,6 +3,28 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+# According to https://cd.uni-freiburg.de/farben/
+blau = "#344A9A"
+dunkelblau = "#00004a"
+sand = "#f6f1e3"
+gruen = "#00a082"
+braun = "#8f6b30"
+gelb = "#ffe863"
+rosa = "#f5c2ed"
+schwarz = "#000000"
+
+COLORS = dict()
+COLORS["nuclear_plant"] = gruen
+COLORS["gas_plant_marginal"] = rosa
+COLORS["marginal"] = rosa
+COLORS["gas_plant_learning"] = dunkelblau
+COLORS["learning"] = dunkelblau
+COLORS["back_up_plant"] = braun
+COLORS["demand"] = schwarz
+
+COLORS["gas_plant_learning_1"] = blau
+COLORS["gas_plant_learning_2"] = dunkelblau
+
 
 def plot_merit_order(df: pd.DataFrame):
 
@@ -31,7 +53,7 @@ def plot_merit_order(df: pd.DataFrame):
           width=row["max_power"],
           align="edge",
           label=row["name"],
-          color=palette[i],
+          color=COLORS[row["name"]],
           edgecolor="black",
           alpha=0.8,
       )
@@ -51,7 +73,7 @@ def plot_demand(df: pd.DataFrame):
   fig, ax = plt.subplots(figsize=(10, 5))
 
   # Plot data
-  sns.lineplot(data=df, x="datetime", y="demand_EOM", ax=ax)
+  sns.lineplot(data=df, x="datetime", y="demand_EOM", ax=ax, color=schwarz)
 
   # Format x-axis hours
   date_form = mdates.DateFormatter("%H")
@@ -61,7 +83,7 @@ def plot_demand(df: pd.DataFrame):
   ax.set_title("Inflexible Demand Curve (EOM)")
 
   index_below_1000 = df[df["demand_EOM"] < 1000]
-  print(index_below_1000.head())
+  
   sns.scatterplot(
       data=index_below_1000,
       x="datetime",
@@ -71,7 +93,7 @@ def plot_demand(df: pd.DataFrame):
       marker="s",
       edgecolor=None,
       label="Demand <= 1000 MW",
-      color="orange") # drawstyle="steps-post")
+      color=gelb) # drawstyle="steps-post")
   # ax.hlines(y=1000, xmin=index_below_1000.min())
 
   plt.tight_layout()
@@ -82,17 +104,33 @@ def plot_demand(df: pd.DataFrame):
 
 def plot_accepted_volume(df: pd.DataFrame):
   date_form = mdates.DateFormatter("%H")
+
+  df["end_time"] = pd.to_datetime(df["end_time"])
+  df["accepted_volume (abs)"] = (df["accepted_volume"].abs())
   
   fig, ax = plt.subplots(1)
+  df_demand = df.query('unit_id == "demand_EOM"')
+  df_generation = df.query('unit_id != "demand_EOM"')
+
+  x_values = df_demand["end_time"]
+  previous = [0] * len(x_values)
+  for label in df_generation["unit_id"].unique():
+    data = df_generation.query('unit_id == @label')
+    y_values = data["accepted_volume (abs)"]
+    # sns.lineplot(data=data, x="end_time", y="accepted_volume (abs)", label=label, color=COLORS[label])
+    ax.plot(x_values, previous, label=label, color=COLORS[label])
+    ax.fill_between(x_values, previous, previous + y_values, color=COLORS[label], alpha=0.45)
+    previous = [prev + y for prev, y in zip(previous, y_values)]
+
+  ax.plot(x_values, df_demand["accepted_volume (abs)"], label="Demand", color=COLORS["demand_EOM"])
   ax.xaxis.set_major_formatter(date_form)
   ax.xaxis.set_major_locator(mdates.HourLocator(interval=4))
-  df["end_time"] = pd.to_datetime(df["end_time"])
   
-  sns.lineplot(data=df, x="end_time", y="accepted_volume", hue="unit_id", alpha=0.75)
-
+  
+  ax.set_ylim(bottom=0)
   ax.set_xlabel("Time")
   ax.set_ylabel("Accepted Volume (MWh)")
-  ax.set_ylim(-10, 1100)
+  
   ax.legend(title="Power Plant")
   plt.tight_layout()
 
@@ -122,7 +160,7 @@ def plot_revenue(df: pd.DataFrame):
 def plot_biddings(df: pd.DataFrame):
   fig, ax = plt.subplots()
   df["end_time"] = pd.to_datetime(df["end_time"])
-  sns.scatterplot(df, x="end_time", y="price", hue="unit_id", ax=ax, marker="X")
+  sns.scatterplot(df, x="end_time", y="price", hue="unit_id", ax=ax, marker="X", palette=COLORS)
   date_form = mdates.DateFormatter('%H')
   ax.xaxis.set_major_formatter(date_form)
   ax.xaxis.set_major_locator(mdates.HourLocator(interval=4))
@@ -137,7 +175,7 @@ def plot_biddings(df: pd.DataFrame):
 def plot_biddings_with_clearing_price(df: pd.DataFrame):
   fig, ax = plt.subplots()
   df["end_time"] = pd.to_datetime(df["end_time"])
-  sns.scatterplot(df, x="end_time", y="price", hue="unit_id", ax=ax, marker="X")
+  sns.scatterplot(df, x="end_time", y="price", hue="unit_id", ax=ax, marker="X", palette=COLORS)
   date_form = mdates.DateFormatter('%H')
   ax.xaxis.set_major_formatter(date_form)
   ax.xaxis.set_major_locator(mdates.HourLocator(interval=4))
@@ -146,7 +184,7 @@ def plot_biddings_with_clearing_price(df: pd.DataFrame):
   ax.legend(title="Strategy")
 
   clearing_price_df = df.groupby('end_time')['accepted_price'].first().reset_index()
-  sns.lineplot(clearing_price_df, y="accepted_price", x="end_time", color="green", alpha=0.8, label="clearing price")
+  sns.lineplot(clearing_price_df, y="accepted_price", x="end_time", color=gelb, alpha=0.8, label="clearing price")
 
   plt.tight_layout()
 
